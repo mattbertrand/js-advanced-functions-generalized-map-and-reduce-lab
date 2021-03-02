ea087d1fd7961dcc5f666cdaac07a0adb9e1626a
github username <github email address>
2021-03-01 17:24:19 -0800

    commit tree c314cdda365b3cb8848640adecf9e2e7b6a191a8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(src, c) {
+    let n = []
+
+    for(let i=0; i<src.length; i++) {
+        let theElement = src[i]
+        n.push(c(theElement))
+    }
+    return n
+}
+
+function reduce(src, c, startingPoint) {
+    let n = (!!startingPoint) ? startingPoint : src[0]
+    let i = (!!startingPoint) ? 0 : 1
+
+    for (; i<src.length; i++) {
+        n = c(src[i], n)
+    }
+    return n
+}
